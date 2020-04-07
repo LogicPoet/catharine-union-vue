e214b6f4392dc4ea87b1448743ea272813ab814d
Liuzhi <zhi.liu@maiunsoft.com>
2020-04-07 16:40:25 +0800

    feature: 添加登陆静态页面

---
+++ src/components/MainContainer.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="MainContainer-container">
+    <h3 class="title">登陆成功</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MainContainer"
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
